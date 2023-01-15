b6afdb42b67f55981388e19c0f2ee3b089c061c0
韦杨婧 <981193371@qq.com>
2023-01-15 20:44:40 +0800

    答辩后改动

--- Consumer.java
+++ Consumer.java
@@ -22,7 +22,7 @@ public class Consumer extends Thread{
 
                 System.out.println(this.getName()+"获得队列的锁");
                 while(queue.isEmpty()){
-                    System.out.println("队列为空，消费者"+this.getName()+"进入等待");
+                    System.out.println("队列为空，"+this.getName()+"进入等待");
                     try{
                         queue.wait();
                     }catch(Exception e){
@@ -31,7 +31,7 @@ public class Consumer extends Thread{
                 }
 
                 int num = queue.poll();
-                System.out.println("消费者"+this.getName()+"消费了"+num+"，队列中还有"+queue.size()+"个产品");
+                System.out.println(this.getName()+"消费了"+num+"，队列中还有"+queue.size()+"个产品");
                 queue.notifyAll();
                 System.out.println(this.getName()+"退出消费，释放队列的锁");
             
--- Producer.java
+++ Producer.java
@@ -22,7 +22,7 @@ public class Producer extends Thread{
                 System.out.println(this.getName()+"获得队列的锁");
 
                 while(queue.size() == maxsize){
-                    System.out.println("队列已满，生产者"+this.getName()+"进入等待");
+                    System.out.println("队列已满，"+this.getName()+"进入等待");
                     try{
                         queue.wait();
                     }catch(Exception e){
@@ -33,7 +33,7 @@ public class Producer extends Thread{
                 int num = (int) (Math.random()*100);
                 queue.offer(num);
 
-                System.out.println("生产者"+this.getName()+"生产了"+num+"，队列中还有"+queue.size()+"个产品");
+                System.out.println(this.getName()+"生产了"+num+"，队列中还有"+queue.size()+"个产品");
                 queue.notifyAll();
 
                 System.out.println(this.getName()+"退出生产，释放队列的锁");
